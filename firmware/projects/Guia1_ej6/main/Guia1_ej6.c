c99b7e655ebd9a14fcafd38724865dd8fe9227a4
EnzoSan23 <enzosanchez237@gmail.com>
2024-04-01 14:34:21 -0300

    Se sube el ejercicio final sin pruebas en placa

--- firmware/projects/Guia1_ej6/main/Guia1_ej6.c
+++ firmware/projects/Guia1_ej6/main/Guia1_ej6.c
@@ -27,8 +27,10 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "gpio_mcu.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 /*==================[macros and definitions]=================================*/
-
+#define duracionPulso 0.00005	//50 nanosegundos
 /*==================[internal data definition]===============================*/
 typedef struct
 {
@@ -68,6 +70,43 @@ void cambioEstadoGPIO(uint8_t digitoBCD, gpioConf_t *vecGPIO)
 /*==================[external functions definition]==========================*/
 void app_main(void)
 {
-	printf("Hello world!\n");
+	uint32_t valor = 258;
+	uint8_t digitos = 3;
+	uint8_t vecBCD[digitos];
+
+	//Defino el vector que mapea los bits
+	gpioConf_t vecBits [4];
+	vecBits[0].pin = GPIO_20;
+	vecBits[1].pin = GPIO_21;
+	vecBits[2].pin = GPIO_22;
+	vecBits[3].pin = GPIO_23;
+	vecBits[0].dir = vecBits[1].dir = vecBits[2].dir = vecBits[3].dir = 1;
+
+	//Defino el vector que mapea los puertos
+	gpioConf_t vecPuertos [3];
+	vecPuertos[0].pin = GPIO_19;
+	vecPuertos[1].pin = GPIO_18;
+	vecPuertos[2].pin = GPIO_9;
+	vecPuertos[0].dir = vecPuertos[1].dir = vecPuertos[2].dir = 1;
+	
+//Inicializo los GPIO
+	for (int i = 0; i < 4; i++)
+	{
+		GPIOInit(vecBits[i].pin, vecBits[i].dir);
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		GPIOInit(vecPuertos[i].pin, vecPuertos[i].dir);
+	}
+
+	convertToBcdArray(valor, digitos, vecBCD);		//Convierto el valor a numeros BCD
+	for (int i = 0; i < digitos; i++)				//Para cada BCD, cargo los bits de entrada del conversor
+	{
+		cambioEstadoGPIO(vecBCD[i], vecBits);
+		GPIOOn(vecPuertos[i].pin);					//Como envio el pulso de 50ns?
+		vTaskDelay(duracionPulso / portTICK_PERIOD_MS);
+		GPIOOff(vecPuertos[i].pin);
+	}
+
 }
 /*==================[end of file]============================================*/
